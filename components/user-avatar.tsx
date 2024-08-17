da2fa64c8e10e5a240ac3c0d37b77402daa7f08c
Antonio Bossor Yankey <antonioyankey770@gmail.com>
2024-08-17 05:18:28 +0000

    Modified code to correct profileImageUrl issue

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -1,15 +1,26 @@
 import { useUser } from "@clerk/nextjs";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-export const UserAvatar =()=>{
-    const {user} =useUser();
-    return(
-         <Avatar className="h-8 w-8">
-        <AvatarImage src={user?.profileImageUrl}/>
-        <AvatarFallback>
-            {user?.firstName?.charAt(0)}
-            {user?.lastName?.charAt(0)}
-        </AvatarFallback>
-         </Avatar>
-    );
+// Define the extended user type if profileImageUrl is missing
+interface ExtendedUser {
+  profileImageUrl?: string;
+  firstName?: string;
+  lastName?: string;
+}
+
+export const UserAvatar = () => {
+  const { user } = useUser();
+
+  // Type assertion to handle the extended properties
+  const extendedUser = user as ExtendedUser;
+
+  return (
+    <Avatar className="h-8 w-8">
+      <AvatarImage src={extendedUser?.profileImageUrl || "/default-avatar.png"} alt={`${extendedUser?.firstName} ${extendedUser?.lastName}`} />
+      <AvatarFallback>
+        {extendedUser?.firstName?.charAt(0) || "U"}
+        {extendedUser?.lastName?.charAt(0) || "A"}
+      </AvatarFallback>
+    </Avatar>
+  );
 };
